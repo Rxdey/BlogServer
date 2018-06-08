8c4eba1da10857b2b597d00d4dd7b1f81c953c1d
rxdey <jyh1994@qq.com>
2018-06-08 15:29:27 +0800

    更改表结构,删除文章详细表

--- app/model/model.js
+++ app/model/model.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = require("../db/db.js")
+const Op = Sequelize.Op;
 const Target = sequelize.define('target', {
   // allowNull defaultValue type autoIncrement primaryKey unique
   id: {
@@ -45,8 +46,68 @@ const Article = sequelize.define('article', {
   tableName: 'article',
 });
 
+const Art = sequelize.define('Art', {
+  // allowNull defaultValue type autoIncrement primaryKey unique
+  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true, unique: true },
+  artHtml:Sequelize.STRING,
+  markdown:Sequelize.TEXT,
+  artId:Sequelize.INTEGER,
+}, {
+  timestamps: false,
+  tableName: 'Art',
+});
+
+
+Target.hasMany(Article,{foreignKey:'id'})
+Article.belongsTo(Target,{foreignKey:'targetId'});
+
+User.hasMany(Target,{foreignKey:'id'});
+Article.belongsTo(User,{foreignKey:'authorId'});
+
+const findPage = async (page=0,target=0,search='',artId=0) => {
+  let pageSize = 10;
+  let last = page*pageSize;
+  let where = { isShow: 1 }
+  if(~~target){
+    where['targetId'] = ~~target;
+  }
+  if(Trim(search)){
+    where = {
+      isShow: 1,
+      title:{
+        [Op.like]: '%'+search+'%'
+      }
+    }
+  }
+  
+  let artList = await Article.findAll({
+    attributes: ['id', 'title', 'image', 'targetId', 'authorId', 'createTime'],
+    include: [{
+      attributes: ['target'],
+      model: Target
+    }, {
+      attributes: ['username'],
+      model: User
+    }, ],
+    where: where,
+    offset: last,
+    limit: pageSize,
+  })
+  return artList;
+}
+
+function Trim(str)
+ { 
+  return str.replace(/(^\s*)|(\s*$)/g, ""); 
+}
+// const getArtList = async () =>{
+//   return await sequelize.query('select a.*,b.target,c.username from article a left join target b on a.targetId = b.id left join user c on c.id = a.authorId');
+// }
+
 module.exports = {
   Target,
   User,
-  Article
+  Article,
+  Art,
+  findPage
 }
--- app/router/index.js
+++ app/router/index.js
@@ -3,7 +3,9 @@ const router = new Router();
 const {
   Target,
   User,
-  Article
+  Article,
+  Art,
+  findPage
 } = require('../model/model')
 router.get('/', async (ctx, next) => {
   let title = 'api'
@@ -11,9 +13,11 @@ router.get('/', async (ctx, next) => {
     title
   })
 });
+
 router.get('/home', async (ctx, next) => {
   ctx.body = ctx.request.query;
 });
+
 router.post('/getTarget', async (ctx, next) => {
   let result = {
     code: '00000',
@@ -37,22 +41,34 @@ router.post('/getArticle', async (ctx, next) => {
   }
   let page = ctx.request.body.page;
   let target = ctx.request.body.target;
-  // let tag = await Target.findAll({where: {isShow: 1}});
-  let artList = await Article.findAll()
+  let search = ctx.request.body.search;
 
+  page<=1&&page?page=0:page;
+
+  let artList = await findPage(page,target,search);
   result.data = artList;
   ctx.body = result;
 });
-const findPage = (page) => {
-  let pageSize = 10;
-  // Article.findAll({
+router.post('/getCon', async (ctx, next) => {
+  // let result = {
+  //   code: '00000',
+  //   data: [],
+  //   msg: '查询成功'
+  // }
+  // let id = ctx.request.body.id;
+  // let list = await Art.findOne({
+  //   attributes:['artHtml'],
   //   include: [{
-  //     model: Target,
-  //     as: 'b',
-  //     where: {
-  //       b: 4
-  //     }
-  //   }]
+  //     attributes:['title','image','createTime','isShow'],
+  //     model: Article
+  //   }],
+  //   where:{
+  //     id:id
+  //   }
   // });
-}
+  // result.data = list;
+  // ctx.body = result;
+});
+
+
 module.exports = router
