b608580ccdf036260edd01d85c3815c549c2fd9b
rxdey <jyh1994@qq.com>
2018-06-09 21:02:27 +0800

    完成文章编辑发布删除等

--- app/model/model.js
+++ app/model/model.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require("../db/db.js")
 const Op = Sequelize.Op;
+const moment = require('moment');
 const Target = sequelize.define('target', {
   // allowNull defaultValue type autoIncrement primaryKey unique
   id: {
@@ -22,92 +23,94 @@ const Target = sequelize.define('target', {
 
 const User = sequelize.define('user', {
   // allowNull defaultValue type autoIncrement primaryKey unique
-  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true, unique: true },
-  username:Sequelize.STRING,
-  passwd:Sequelize.STRING,
-  headimg:Sequelize.TEXT,
-  createTime:Sequelize.DATE
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true
+  },
+  username: Sequelize.STRING,
+  passwd: Sequelize.STRING,
+  headimg: Sequelize.TEXT,
+  createTime: Sequelize.STRING
 }, {
   timestamps: false,
   tableName: 'user',
 });
 const Article = sequelize.define('article', {
   // allowNull defaultValue type autoIncrement primaryKey unique
-  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true, unique: true },
-  title:Sequelize.STRING,
-  image:Sequelize.TEXT,
-  createTime:Sequelize.DATE,
-  targetId:Sequelize.INTEGER,
-  authorId:Sequelize.INTEGER,
-  art:Sequelize.TEXT,
-  isShow: Sequelize.INTEGER
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    unique: true
+  },
+  title: Sequelize.STRING,
+  image: Sequelize.TEXT,
+  createTime: Sequelize.STRING,
+  targetId: Sequelize.INTEGER,
+  authorId: Sequelize.INTEGER,
+  artHtml: Sequelize.TEXT,
+  artMarkDown: Sequelize.TEXT,
+  isShow: Sequelize.INTEGER,
+  isPub: Sequelize.INTEGER
 }, {
   timestamps: false,
   tableName: 'article',
 });
 
-const Art = sequelize.define('Art', {
-  // allowNull defaultValue type autoIncrement primaryKey unique
-  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true, unique: true },
-  artHtml:Sequelize.STRING,
-  markdown:Sequelize.TEXT,
-  artId:Sequelize.INTEGER,
-}, {
-  timestamps: false,
-  tableName: 'Art',
+Target.hasMany(Article, {
+  foreignKey: 'id'
+})
+Article.belongsTo(Target, {
+  foreignKey: 'targetId'
+});
+User.hasMany(Target, {
+  foreignKey: 'id'
+});
+Article.belongsTo(User, {
+  foreignKey: 'authorId'
 });
 
 
-Target.hasMany(Article,{foreignKey:'id'})
-Article.belongsTo(Target,{foreignKey:'targetId'});
-
-User.hasMany(Target,{foreignKey:'id'});
-Article.belongsTo(User,{foreignKey:'authorId'});
-
-const findPage = async (page=0,target=0,search='',artId=0) => {
-  let pageSize = 10;
-  let last = page*pageSize;
-  let where = { isShow: 1 }
-  if(~~target){
-    where['targetId'] = ~~target;
-  }
-  if(Trim(search)){
-    where = {
-      isShow: 1,
-      title:{
-        [Op.like]: '%'+search+'%'
-      }
-    }
-  }
+const findPage = async ({page = 0, target = 0, search = '', artId = 0,type=0,isPub = 1}={}) => {
+    let pageSize = 10;
+    let last = page * pageSize;
+    let attributes = ['id', 'title', 'image', 'targetId', 'authorId', 'createTime','isPub']
+    let where = { isShow: 1}
+    if(~~isPub)where['isPub'] = ~~isPub
   
-  let artList = await Article.findAll({
-    attributes: ['id', 'title', 'image', 'targetId', 'authorId', 'createTime'],
-    include: [{
-      attributes: ['target'],
-      model: Target
-    }, {
-      attributes: ['username'],
-      model: User
-    }, ],
-    where: where,
-    offset: last,
-    limit: pageSize,
-  })
-  return artList;
-}
-
-function Trim(str)
- { 
-  return str.replace(/(^\s*)|(\s*$)/g, ""); 
-}
-// const getArtList = async () =>{
-//   return await sequelize.query('select a.*,b.target,c.username from article a left join target b on a.targetId = b.id left join user c on c.id = a.authorId');
-// }
-
-module.exports = {
-  Target,
-  User,
-  Article,
-  Art,
-  findPage
-}
+    if (~~target) where['targetId'] = ~~target
+    if (Trim(search)) where['title'] = { [Op.like]: '%' + search + '%' } 
+    if(~~artId){
+      where['id'] = ~~artId;
+      if(~~type === 2)attributes.push('artHtml')
+      if(~~type === 1)attributes.push('artMarkDown')
+    }
+    const artList = await Article.findAll({
+      attributes: attributes,
+      include: [{
+        attributes: ['target'],
+        model: Target
+      }, {
+        attributes: ['username','headimg'],
+        model: User
+      }, ],
+      where: where,
+      offset: last,
+      limit: pageSize,
+    })
+    return artList
+  }
+    function Trim(str) {
+      return str.replace(/(^\s*)|(\s*$)/g, "");
+    }
+  function fromateDate(string){
+    return moment(string).format().toString().replace(/T/g,' ').replace(/\.[\d]{3}Z/,'')
+  }
+    module.exports = {
+      Target,
+      User,
+      Article,
+      findPage
+    }
--- app/router/index.js
+++ app/router/index.js
@@ -1,10 +1,10 @@
 const Router = require('koa-router');
+const moment = require('moment');
 const router = new Router();
 const {
   Target,
   User,
   Article,
-  Art,
   findPage
 } = require('../model/model')
 router.get('/', async (ctx, next) => {
@@ -42,32 +42,134 @@ router.post('/getArticle', async (ctx, next) => {
   let page = ctx.request.body.page;
   let target = ctx.request.body.target;
   let search = ctx.request.body.search;
+  let type = ctx.request.body.isPub;
 
-  page<=1&&page?page=0:page;
+  page <= 1 && page ? page = 0 : page;
 
-  let artList = await findPage(page,target,search);
+  let artList = await findPage({
+    page: page,
+    target: target,
+    search: search,
+    isPub: type
+  });
   result.data = artList;
   ctx.body = result;
 });
+
 router.post('/getCon', async (ctx, next) => {
-  // let result = {
-  //   code: '00000',
-  //   data: [],
-  //   msg: '查询成功'
-  // }
-  // let id = ctx.request.body.id;
-  // let list = await Art.findOne({
-  //   attributes:['artHtml'],
-  //   include: [{
-  //     attributes:['title','image','createTime','isShow'],
-  //     model: Article
-  //   }],
-  //   where:{
-  //     id:id
-  //   }
-  // });
-  // result.data = list;
-  // ctx.body = result;
+  let result = {
+    code: '00000',
+    data: [],
+    msg: '查询成功'
+  }
+  let id = ctx.request.body.id;
+  let type = ctx.request.body.type;
+  let list = await findPage({
+    artId: id,
+    type: type,
+    isPub: 0
+  });
+  result.data = list;
+  ctx.body = result;
+});
+
+router.post('/saveArt', async (ctx, next) => {
+  let result = {
+    code: '00000',
+    data: [],
+    msg: '保存成功'
+  }
+  let id = ctx.request.body.id;
+  let title = ctx.request.body.title;
+  let artMarkDown = ctx.request.body.artMarkDown;
+  let artHtml = ctx.request.body.artHtml;
+
+  let success = await Article.update({
+    title: title,
+    artMarkDown: artMarkDown,
+    artHtml: artHtml
+  }, {
+    where: {
+      id: id,
+    }
+  })
+  result.data = success;
+  ctx.body = result
+});
+router.post('/deleteTarget', async (ctx, next) => {
+  let id = ctx.request.body.id;
+  let success = await Target.update({
+    isShow: 0
+  }, {
+    where: {
+      id: id,
+    }
+  })
+  await Article.update({
+    isShow: 0
+  }, {
+    where: {
+      targetId: id,
+    }
+  })
+  ctx.body = success
+});
+router.post('/delArt', async (ctx, next) => {
+  let id = ctx.request.body.id;
+  let success = await Article.update({
+    isShow: 0
+  }, {
+    where: {
+      id: id,
+    }
+  })
+  ctx.body = success
+});
+
+router.post('/addTaget', async (ctx, next) => {
+  let result = {
+    code: '00000',
+    data: [],
+    msg: '添加成功'
+  }
+  let name = ctx.request.body.target;
+  let artMarkDown = ctx.request.body.artMarkDown;
+  let success = await Target.create({
+    target: name
+  });
+  result.data = success;
+  ctx.body = result
+});
+
+router.post('/addArticle', async (ctx, next) => {
+  let result = {
+    code: '00000',
+    data: [],
+    msg: '添加成功'
+  }
+  let targetId = ctx.request.body.targetId;
+  let success = await Article.create({
+    targetId: targetId,
+    createTime: moment(new Date()).format(),
+    authorId: 1
+  });
+  result.data = success;
+  ctx.body = result
+});
+
+
+router.post('/pubArt', async (ctx, next) => {
+  let id = ~~ctx.request.body.id;
+  let isPub = ~~ctx.request.body.isPub;
+  console.log(isPub, id)
+  let success = await Article.update({
+    isPub: isPub
+  }, {
+    where: {
+      id: id,
+    }
+  })
+  ctx.body = success
 });
 
 
